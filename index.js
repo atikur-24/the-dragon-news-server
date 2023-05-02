215d3b545c15d6fe514b2ee7880b7e975616d3cb
Atikur <atikurrahman7223@gmail.com>
2023-05-02 15:26:24 +0600

    all  news data, single news and category news data are load in server side

--- index.js
+++ index.js
@@ -4,15 +4,38 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
+
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('hello dragon news')
+    res.send('hello dragon news');
 })
 
 app.get('/categories', (req, res) => {
-    res.send(categories)
+    res.send(categories);
 })
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id === 0) {
+        res.send(news)
+    }
+    else {
+        const categoryNew = news.filter(n => parseInt(n.category_id) === id);
+        res.send(categoryNew);
+    }
+}) 
 
 app.listen(port, () => {
     console.log(`The news dragon in running on port ${port}`);
